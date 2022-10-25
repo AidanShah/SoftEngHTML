cc3b01379af0fe4e75061021b3b5d0b52d6ba6c3
Aidan Shah <aidanshah@Aidans-MacBook-Pro.local>
2022-10-25 09:45:22 -0400

    more javascript

--- javascript/aidan.js
+++ javascript/aidan.js
@@ -1,8 +1,2 @@
 //2
-pi.onclick= document.getElementById('no').innerHTML="You pressed a button!";;
-//3
-function Count(){
-    
-}
-count.onclick= Count();
-console.log("Work")
+pi.onclick = function(){document.getElementById('no').innerHTML="You pressed a button!";}
