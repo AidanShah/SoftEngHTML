d8fee8b55818c5c014b9c37266972f72ba5332ed
Aidan Shah <aidanshah@Aidans-MacBook-Pro.local>
2022-10-27 09:29:17 -0400

    javascript

--- javascript/aidan.js
+++ javascript/aidan.js
@@ -4,13 +4,10 @@ pi.onclick= function addElement() {
     const tag = document.createElement("p");
     const text = document.createTextNode("You pressed a button!\n");
     tag.appendChild(text);
-  
-    // add the newly created element and its content into the DOM
     const currentDiv = document.getElementById("div1");
     document.body.insertBefore(tag, currentDiv);
   }
 //3
-//on click gets the h4 text, then increments by 1
 count.onclick = function(){document.getElementById('counting').innerHTML=1+parseInt(document.getElementById('counting').innerHTML);}
 //4
 blue.onclick = function changeBlue(){
@@ -28,13 +25,8 @@ enter.onclick = function repeatText(){
     document.getElementById("enterText").innerHTML= text;
 }
 //6
-fots.onclick = function changeTimes(){
+fonts.onchange = function changeTimes(){
     selectElement = document.querySelector('#fonts');
     output = selectElement.value;
     document.getElementById("fontText").style.fontFamily = output;
-    const tag = document.createElement("p");
-    const text = document.createTextNode(output);
-    tag.appendChild(text);
-    const currentDiv = document.getElementById("div1");
-    document.body.insertBefore(tag, currentDiv);
 }
